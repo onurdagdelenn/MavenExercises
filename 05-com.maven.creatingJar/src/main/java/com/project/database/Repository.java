bd4bb1353e3e7af48757d382db1eb6c827baef60
Onur Dağdelen <onurdagdelenn@gmail.com>
2020-02-23 21:24:55 +0300

    first commit

---
+++ 05-com.maven.creatingJar/src/main/java/com/project/database/Repository.java
@@ -0,0 +1,29 @@
+package com.project.database;
+
+public class Repository {
+	
+	
+	public void insert(String ad) {
+		
+ System.out.println(ad);
+		
+	}
+	
+	public void delete(String delete) {
+		
+		System.out.println(delete);
+		
+	}
+	
+	public void update() {
+		
+		System.out.println("Güncellendi");
+		
+	}
+	
+	
+	
+	
+	
+
+}
